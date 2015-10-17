a4545b5b65befc8c084626774bd1a0878fa09266
Charles Schultz <sacrophyte@gmail.com>
2015-10-16 21:52:00 -0500

    insert the choice data

--- insert_from_imported_tables.sql
+++ insert_from_imported_tables.sql
@@ -24,7 +24,6 @@ group by `UNIT 4`, `TEST 2`) a )
 -- add an entry for an 'unassigned' school (some data elements use 0 to indicate the unassigned status
 insert into  `schools` values (0,'No school','N/A');
 
-
 insert into `school_capacity` (
     select school_id,
 '2012',
@@ -35,3 +34,43 @@ FROM `SC`
 WHERE `UNIT 4` is not null and `UNIT 4` != 'CODE' and `UNIT 4` != 'TOTALS:'
 group by `UNIT 4`, `TEST 2`) a )
 ;
+
+-- insert the preassignments
+insert into choices (
+select '2012' year,
+ null choice1,
+ null choice2,
+ null choice3,
+ null choice4,
+ null choice5,
+ null choice6,
+ null choice7,
+ null choice8,
+ null choice9,
+ null choice10,
+ null choice11,
+ null choice12,
+ preasg assigned_school
+from `apps2012_run3`
+where preasg != 0)
+;
+
+-- insert the "real" choices
+insert into choices (
+select '2012' year,
+ ch1sch choice1,
+ ch2sch choice2,
+ ch3sch choice3,
+ ch4sch choice4,
+ ch5sch choice5,
+ null choice6,
+ null choice7,
+ null choice8,
+ null choice9,
+ null choice10,
+ null choice11,
+ null choice12,
+ asgsch assigned_school
+from `apps2012_run3`
+where preasg = 0
+);
